388654e00b501882b55afd17d502f68bf032bed9
thanh-do28 <96042778+thanh-do28@users.noreply.github.com>
2022-07-28 18:35:24 +0700

    ss1_Homework bài tại nha

---
+++ baitap.js
@@ -0,0 +1,29 @@
+// bài 1 : viêt chương trình nhập điểm của 1 sinh viên cho các môn
+// let nhapdiem = 'nhập điểm của học sinh';
+// alert(nhapdiem);
+// let vatly = Number(prompt('nhập điểm vật lý'));
+// let hoahoc = Number(prompt('nhập điểm hóa học'));
+// let sinhhoc = Number(prompt('nhập điểm sinh học'));
+// let sum = vatly + hoahoc + sinhhoc;
+// alert('tổng điểm là : ' + sum);
+// let tbinh = sum / 3;
+// alert('điểm trung bình là : ' + tbinh);
+
+
+// // bài 2 : viết chương trình chuyển đổi độ
+// let Celsius = Number(prompt('nhập Celsius'));
+// let Fahrenheit = 1.8 * Celsius + 32;
+// alert('Fahrenheit là : ' + Fahrenheit);
+
+
+
+// // bài 3 : viết chương trình tính diện tích hình tròn
+// let bankinh = Number(prompt('nhập bán kính hình tròn'));
+// let dientich = (bankinh**2) * 3.14;
+// alert('diện tích hình tròn là : ' + dientich);
+
+
+// // bài 4 : viết chương trình tính chu vi hình tròn
+// let bankinh = Number(prompt('nhập bán kính hình tròn'));
+// let chuvi = (2*bankinh) * 3.14;
+// alert('chu vi hình tròn là : ' + chuvi);
